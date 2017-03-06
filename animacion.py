88446cc85b5543e5336542b53854bfc66e5d0225
nathan <mateomarin97@hotmail.com>
2017-03-06 11:03:34 -0500

    Python grafica energias

--- animacion.py
+++ animacion.py
@@ -2,8 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 datos= np.loadtxt("datos.dat")
-a = datos.T
+e1=datos[:,0].T
+e2=datos[:,1].T
+e3=datos[:,2].T
+
+x=np.linspace(0,100,len(e1))
 plt.figure(figsize=(20,20))
-plt.imshow(a)
+plt.plot(x,e1,label='e1')
+plt.plot(x,e2,label='e2')
+plt.plot(x,e3,label='e3')
+plt.legend()
 plt.savefig("grafico.pdf")
 
--- cuerda.c
+++ cuerda.c
@@ -3,13 +3,16 @@
 #include <math.h>
 #include "omp.h"
 #define N 64
-#define T 1000
+#define T 100
 #define b 1.0
 #define dt 0.005
 #define pi 3.141592654
 float inicializar(float x);
 void leapfrog(float *yp,float *v);
 float vder(int i,float *yp);
+float A(int k, float *yp);
+float Ap(int k, float *v);
+float w2(int k);
 
 int main(){
   /*Inicializo la lista de las possicones y las velocidades*/
@@ -45,11 +48,38 @@ float inicializar(float x){
   return sin(pi*(float)x/(float)(N-1));
 }
 
+float A(int k, float *yp){
+  float suma=0;
+  int i;
+  for(i=0;i<N;i++){
+    suma+= yp[i]*sin((i*k*pi)/(float)(N+1));
+  }
+  return pow(2.0/(N+1),0.5)*suma;
+  
+}
+
+float Ap(int k, float *v){
+  float suma=0;
+  int i;
+  for(i=0;i<N;i++){
+    suma+= v[i]*sin((i*k*pi)/(float)(N+1));
+  }
+  return pow(2.0/(N+1),0.5)*suma;
+
+}
+/*Funcion omega al cuadrado*/
+float w2(int k){
+  return 4.0*pow(sin((k*pi)/((2.0*N)+2.0)),2.0);
+
+}
+
+
 void leapfrog(float *yp,float *v){
   int Nt= (int)(T/dt);
   float *vi;
   int i;
   int k;
+  int modulo = (int)(Nt/1000);
   omp_set_num_threads(1);
 
   vi=malloc(N*sizeof(float));
@@ -70,15 +100,11 @@ void leapfrog(float *yp,float *v){
        for(k=0;k<N;k++){
 	 v[k]=vi[k]+(vder(k,yp)*(dt/2.0));
        }
-    if(i%1000==0){
-        for(k=0;k<N;k++){
-	  if(k==N-1){
-	    printf("%f \n ",yp[k]);
-	  }
-	  else{
-	    printf("%f ",yp[k]);
-	  }
-	}
+    if(i%modulo==0){
+      printf("%f ",(pow(Ap(1,v),2.0)+w2(1)*pow(A(1,yp),2.0))/2.0);
+      printf("%f ",(pow(Ap(2,v),2.0)+w2(2)*pow(A(2,yp),2.0))/2.0);
+      printf("%f \n ",(pow(Ap(3,v),2.0)+w2(3)*pow(A(3,yp),2.0))/2.0);
+	
 
     }
   }
